dfd1689636f9c5cc9b14c0ab9ad0aa19bc85f2fb
wangpeibao <wangpeibao@troila.com>
2020-03-18 16:32:22 +0800

    模块化响应时长，sql次数

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_redis import FlaskRedis
+from extend.api_duration import ApiDuration
 
 db = SQLAlchemy()
 redis = FlaskRedis()
+api_duration = ApiDuration()
 
 
 # 创建app
@@ -15,6 +17,7 @@ def create_app(config):
     db.init_app(app)
     db.app = app
     redis.init_app(app)
+    api_duration.init_app(app, db)
 
     # 注册蓝图
     from .app_api import api as blue_app_api
---
+++ extend/api_duration/__init__.py
@@ -0,0 +1,35 @@
+# 记录sql查询时间和接口访问时间
+from datetime import datetime
+from flask import request
+from flask_sqlalchemy import get_debug_queries
+
+
+class ApiDuration(object):
+    def __init__(self, app=None, db=None):
+        self.app = app
+        self.db = db
+
+    def init_app(self, app, db):  # 初始化，根据需求到是否
+        self.app = app
+        self.db = db
+
+        # API接口响应时间
+        class ApiTime(db.Model):
+            object_id = db.Column(db.Integer, primary_key=True)
+            create_time = db.Column(db.DateTime, default=datetime.now)
+            path = db.Column(db.String(100))
+            duration = db.Column(db.Integer)
+            sql_count = db.Column(db.Integer, default=0)
+
+        @self.app.before_request
+        def insert_start_time():
+            request.start_time = datetime.now().timestamp()
+
+        @self.app.after_request
+        def get_request_time(response):
+            if app.config["DEBUG"]:
+                length = datetime.now().timestamp() - request.start_time
+                api = ApiTime(path=request.path, duration=int(length * 1000), sql_count=len(get_debug_queries()))
+                db.session.add(api)
+                db.session.commit()
+            return response
